3b929967e0c1c193097c7ce50cb3307fa8ff745b
tsundoku-white <chrisventer999@gmail.com>
2026-09-03 19:52:02 +0800

    added: buffer.cc/h

--- src/vulkan/buffer.cc
+++ src/vulkan/buffer.cc
@@ -1,1 +1,112 @@
 #include "buffer.h"
+
+#include "src/vulkan/context.h"
+#include "src/vulkan/command.h"
+
+namespace n_buffer 
+{
+  void create_buffer(Buffer &buffer, Context &context, VkDeviceSize size,
+      VkBufferUsageFlags usage, VmaMemoryUsage memory_usage,
+      VmaAllocationCreateFlags alloc_flags)
+  {
+    VkBufferCreateInfo bufferInfo{};
+    bufferInfo.sType       = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size        = size;
+    bufferInfo.usage       = usage;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    VmaAllocationCreateInfo allocInfo{};
+    allocInfo.usage = memory_usage;
+    allocInfo.flags = alloc_flags;
+
+    VmaAllocationInfo nAllocInfo{};
+
+    if (vmaCreateBuffer(context.m_allocator, &bufferInfo, &allocInfo,
+          &buffer.m_buffer, &buffer.m_allocation, &nAllocInfo) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create buffer");
+    }
+
+    buffer.m_size   = size;
+    buffer.m_mapped = nAllocInfo.pMappedData;
+  }
+
+  static void copyBuffer(Context &context, Command &command, VkBuffer src, VkBuffer dst, VkDeviceSize size)
+  {
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType              = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.level              = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandPool        = command.m_pool;
+    allocInfo.commandBufferCount = 1;
+
+    VkCommandBuffer cmd;
+    vkAllocateCommandBuffers(context.m_device, &allocInfo, &cmd);
+
+    VkCommandBufferBeginInfo beginInfo{};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+    vkBeginCommandBuffer(cmd, &beginInfo);
+
+    VkBufferCopy copyRegion{};
+    copyRegion.size = size;
+    vkCmdCopyBuffer(cmd, src, dst, 1, &copyRegion);
+
+    vkEndCommandBuffer(cmd);
+
+    VkSubmitInfo submitInfo{};
+    submitInfo.sType              = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers    = &cmd;
+
+    vkQueueSubmit(context.m_queue, 1, &submitInfo, VK_NULL_HANDLE);
+    vkQueueWaitIdle(context.m_queue); 
+
+    vkFreeCommandBuffers(context.m_device, command.m_pool, 1, &cmd);
+  }
+
+
+  void createVertexBuffer(Buffer &buffer, Command &command, Context &context, std::vector<Vertex> vertices)
+  {
+    VkDeviceSize size = sizeof(Vertex) * vertices.size();
+
+    Buffer staging{};
+    create_buffer(staging, context, size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VMA_MEMORY_USAGE_AUTO,
+        VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT);
+
+    std::memcpy(staging.m_mapped, vertices.data(), size);
+
+    create_buffer(buffer, context, size, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        VMA_MEMORY_USAGE_AUTO, 0);
+
+    copyBuffer(context, command, staging.m_buffer, buffer.m_buffer, size);
+    destroyBuffer(staging, context);
+  }
+
+  void createIndexBuffer(Buffer &buffer, Command &command, Context &context, std::vector<uint32_t> indices)
+  {
+    VkDeviceSize size = sizeof(uint32_t) * indices.size();
+
+    Buffer staging{};
+    create_buffer(staging, context, size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VMA_MEMORY_USAGE_AUTO,
+        VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT);
+
+    std::memcpy(staging.m_mapped, indices.data(), size);
+
+    create_buffer(buffer, context, size, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+        VMA_MEMORY_USAGE_AUTO, 0);
+
+    copyBuffer(context, command, staging.m_buffer, buffer.m_buffer, size);
+    destroyBuffer(staging, context);
+  }
+
+  void destoryBuffer(Buffer &buffer, Context &context)
+  {
+    if (buffer.m_buffer == VK_NULL_HANDLE) return;
+    vmaDestroyBuffer(context.m_allocator, buffer.m_buffer, buffer.m_allocation);
+    buffer.m_buffer     = VK_NULL_HANDLE;
+    buffer.m_allocation = VK_NULL_HANDLE;
+    buffer.m_mapped     = nullptr;
+  }
+
+}
--- src/vulkan/buffer.h
+++ src/vulkan/buffer.h
@@ -1,12 +1,46 @@
 #pragma once
 
+#include "src/core/pch.h"
+
+struct Context;
+struct Command;
+
+struct alignas(16) UniformBufferObject
+{
+  // camera projection
+  glm::mat4 m_projection_matrix;
+  glm::mat4 m_view_matrix;
+};
+
+struct alignas(16) ShaderStorageBufferObject
+{
+  // models
+  glm::mat4 m_models_matrix = glm::mat4(1.f);
+  size_t    m_model_count   = 0;
+
+  // textures
+  int32_t   m_indices       = 0;
+  size_t    m_indices_count = 0;
+};
+
+struct Vertex {
+  glm::vec3 m_pos;
+  glm::vec3 m_normal;
+  glm::vec2 m_uv;
+};
+
 struct Buffer
 {
-
+  VkBuffer m_buffer;
+  VmaAllocation m_allocation;
+  VmaAllocationInfo m_info;
+  VkDeviceSize  m_size       = 0;
+  void*         m_mapped     = nullptr;
 };
 
 namespace n_buffer
 {
-  void create_buffer(Buffer &buffer);
-  void destory_context(Buffer &buffer);
+  void createVertexBuffer(Buffer &buffer, Command &command, Context &context, std::vector<Vertex> vertices);
+  void createIndexBuffer(Buffer &buffer, Command &command, Context &context, std::vector<uint32_t> indices);
+  void destroyBuffer(Buffer &buffer, Context &context);
 }
